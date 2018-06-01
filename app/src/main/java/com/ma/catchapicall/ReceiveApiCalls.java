2b32365ab8d889440a39de305362e20d954d9268
amatanat <ahmadova.matanat01@gmail.com>
2018-06-01 10:36:30 +0200

    refactor: log API calls.
    Log API calls for the ContactSelectionActivity.
    Log API calls for the AllInOneActivity in Calendar app.

--- app/src/main/java/com/ma/catchapicall/ReceiveApiCalls.java
+++ app/src/main/java/com/ma/catchapicall/ReceiveApiCalls.java
@@ -1,16 +1,6 @@
 package com.ma.catchapicall;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.text.TextUtils;
-import android.widget.TextView;
-import android.support.v7.app.AppCompatActivity;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -20,46 +10,78 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 
+/**
+ * Created by amatanat
+ */
+
 public class ReceiveApiCalls implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        if (lpparam.packageName.equals("com.android.systemui")){
-            XposedBridge.log("%%%%%%%%%%%%%%%% Loaded app: " + lpparam.packageName);
-            findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-                    TextView tv = (TextView) param.thisObject;
-                    String text = tv.getText().toString();
-                    XposedBridge.log("time: " + text);
-                    tv.setText(text + " :)");
-                    tv.setTextColor(Color.RED);
-                }
-            });
-//            // TODO before a method call display a new UI.
-
-        } else if(lpparam.packageName.equals("com.android.contacts")) {
+            // check the package name
+            if (lpparam.packageName.equals("com.android.contacts")) {
             XposedBridge.log("******Loaded app: " + lpparam.packageName);
             XposedBridge.log("@@@@@@@@@ found class: " + findClass
                     ("com.android.contacts.activities.ContactSelectionActivity",
-                    lpparam.classLoader));
+                            lpparam.classLoader));
 
+            // find 'onCreate' in ContactSelectionActivity and hook it.
             findAndHookMethod("com.android.contacts.activities.ContactSelectionActivity", lpparam.classLoader,
-                    "onCreate", Bundle.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) {
-                    Context context = (Context) (param.thisObject);
-                    PackageManager manager = context.getPackageManager();
-                    Intent intent = manager.getLaunchIntentForPackage("com.ma.bakingrecipes");
-                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                    context.startActivity(intent);
-                    XposedBridge.log("@@@@@@@@@ " + "intent is started");
-                }
-            });
+                    "onCreate", Bundle.class,
 
-        }else{
-            XposedBridge.log("----------------------- Loaded app: " + lpparam.packageName);
-            return;
-        }
+                    // replace method in ContactSelectionActivity
+//                    new XC_MethodReplacement() {
+//                        @Override
+//                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+//                            Context context = (Context) (param.thisObject);
+//                            PackageManager manager = context.getPackageManager();
+//                            // open third-party app
+//                            Intent intent = manager.getLaunchIntentForPackage("com.ma.bakingrecipes");
+//                            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//                            // TODO startActivityForResult
+//                            context.startActivity(intent);
+//                            // TODO return required value
+//                            return null;
+//                        }
+//                    }
+
+                    // before onCreate call in ContactSelectionActivity.
+                    new XC_MethodHook() {
+                @Override
+                // before 'onCreate' call in ContactSelectionActivity, log the text
+                protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) {
+                    XposedBridge.log("*** Xposed log before onCreate call in Contact.");
+                }
+            } );
+        } else if(lpparam.packageName.equals("com.android.calendar")){
+                XposedBridge.log("******Loaded app: " + lpparam.packageName);
+
+                findAndHookMethod("com.android.calendar.AllInOneActivity", lpparam.classLoader,
+                        "onCreate", Bundle.class,
+
+                        // replace method in ContactSelectionActivity
+//                    new XC_MethodReplacement() {
+//                        @Override
+//                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+//                            Context context = (Context) (param.thisObject);
+//                            PackageManager manager = context.getPackageManager();
+//                            // open third-party app
+//                            Intent intent = manager.getLaunchIntentForPackage("com.ma.bakingrecipes");
+//                            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+//                            // TODO startActivityForResult
+//                            context.startActivity(intent);
+//                            // TODO return required value
+//                            return null;
+//                        }
+//                    }
+
+                        // before onCreate call in ContactSelectionActivity.
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) {
+                                XposedBridge.log("*** Xposed log before onCreate call in Calendar.");
+                            }
+                        } );
+            }
     }
 }
